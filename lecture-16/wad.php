854be18544bf5c1c5a84afd8416148deabc3b9be
HyderAshfaq <44546094+HyderAshfaq@users.noreply.github.com>
2018-12-26 04:54:03 -0800

    Add files via upload

---
+++ lecture-16/wad.php
@@ -0,0 +1,10 @@
+<?php
+$s=array("Benefits of tree...","Clean air...","Environmental ","Economic Benefits");
+echo"<h1>$s[0]</h1>";
+for($x=0;$x <4;$x++)
+{
+	echo"<li>";
+		echo $s[$x];
+	echo"</li>";
+}
+?>
